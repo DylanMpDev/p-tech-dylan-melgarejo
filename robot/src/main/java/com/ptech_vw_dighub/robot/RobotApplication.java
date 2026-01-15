4aee41b79a31c031c7d8fc3a6154cbb0e590504f
jdylanmp <dylanrockbcn@gmail.com>
2026-01-15 13:11:38 +0100

    Initial commit before start p tech

---
+++ robot/src/main/java/com/ptech_vw_dighub/robot/RobotApplication.java
@@ -0,0 +1,13 @@
+package com.ptech_vw_dighub.robot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RobotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RobotApplication.class, args);
+	}
+
+}
